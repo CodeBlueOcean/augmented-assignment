239b72ef501f25b7645e206c49d03f6f82361e37
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-18 22:38:00 -0600

    Add files via upload

---
+++ augmented-assignment.py
@@ -0,0 +1,15 @@
+# augmented assignment operator
+some_value = 5
+some_value = some_value + 2
+
+print(some_value)
+
+#fast way
+
+some_value = 5
+some_value += + 2
+
+print(some_value)
+
+some_value = some_value * 2
+print
